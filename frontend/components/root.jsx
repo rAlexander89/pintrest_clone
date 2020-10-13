bd85e6d5d911f6edec3c5e90b8f4d10e5494b6db
Mac Mini <ryan@cygaq.com>
2020-10-13 16:48:10 -0700

    added app.jsx

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
-import App from './App';
+import App from './app';
 
 // NB: this file is complete - you do not to write/edit anything!
 const Root = ({ store }) => (
