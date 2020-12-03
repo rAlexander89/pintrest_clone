f735d9f9f6c8921729ee9f8a8452b56252ab3c66
Mac Mini <ryan@cygaq.com>
2020-12-03 11:54:08 -0800

    cleaned up navbar/session links

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 const Greeting = ({ currentUser, logout, openModal}) => {
 
     
-    const sessionLinks = () => (
-        <ul id="login-signup">
-            <li><button id='loginbtn' onClick={() => openModal('login')}>Log in</button></li>
-            <li><button id='signinbtn' onClick={() => openModal('signup')}>Sign up</button></li>
-        </ul>
-    );
-    <div>
+    // const sessionLinks = () => (
+    //     <ul id="login-signup">
+    //         <li><button id='loginbtn' onClick={() => openModal('login')}>Log in</button></li>
+    //         <li><button id='signinbtn' onClick={() => openModal('signup')}>Sign up</button></li>
+    //     </ul>
+    // );
+    // <div>
 
-    </div>
+    // </div>
     const personalGreeting = () => (    
         <div>
             <ul id="login-signup">
@@ -21,13 +21,14 @@ const Greeting = ({ currentUser, logout, openModal}) => {
                 <li><Link to='/pins/'>Pins</Link></li>
                 <li><Link to={`/users/${currentUser.id}`}>My Profile</Link></li>
                 <li><Link to='/p/create'><button id='make-pin'>Make a pin</button></Link></li>
-                <li><button id='make-board'>Make a board</button></li>
+                <li><Link to='/b/create'><button id='make-board'>Make a board</button></Link></li>
                 <li><Link to='/'><button id="logoutbtn" onClick={logout}>Log Out</button></Link></li>
             </ul>
         </div>
     );
 
-    return currentUser ? personalGreeting() : sessionLinks();
+    // return currentUser ? personalGreeting() : sessionLinks();
+    return currentUser ? personalGreeting() : null;
 };
 
 
