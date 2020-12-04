b2db4446661ea6d71db3bd15c9aeecb91438f3fa
Mac Mini <ryan@cygaq.com>
2020-12-04 15:21:54 -0800

    fixed error where pin creation would not take you to the pin show page after a pin was created

--- frontend/reducers/selector.js
+++ frontend/reducers/selector.js
@@ -4,13 +4,13 @@ export const selectBoardPins = (boardPins, pins, boardId) => {
     if (Object.values(pins).length === 0) return null;
     let pinsOnBoard = [];
     Object.values(boardPins).forEach(boardPin => {
+        debugger
         if (boardPin.board_id === parseInt(boardId)){
             if (!!pins[boardPin.pin_id]){
                 pinsOnBoard.push(pins[boardPin.pin_id])
             }
         }
     })
-
     return pinsOnBoard
 }
 
