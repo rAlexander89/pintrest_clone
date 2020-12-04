cd9d8e5382a41b586c81e312d5bf8c3941b60370
Mac Mini <ryan@cygaq.com>
2020-12-03 21:30:15 -0800

    created errors reducers

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,7 +1,14 @@
 import { combineReducers } from 'redux';
 
 import session from './session_errors_reducer';
+import users from './users_errors_reducer';
+import pins from './pins_errors_reducer';
+import boards from './boards_errors_reducer';
+
 
 export default combineReducers({
-    session
+    session,
+    users,
+    pins,
+    boards
 });
