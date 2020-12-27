93501a3a3595368072535aa7716528d98c0ae11e
Mac Mini <ryan@cygaq.com>
2020-12-26 23:16:41 -0800

    fixed redirect after succesful deletion of board

--- frontend/components/board/board_show.jsx
+++ frontend/components/board/board_show.jsx
@@ -1,25 +1,96 @@
-import React from 'react';
+import React from 'react'
 import PinIndexItem from '../pin/pin_index_item';
 import InlineDisplay from '../inline_display/inline_display_container'
+import Swal from 'sweetalert2'
 
 
 class BoardShow extends React.Component {
     constructor(props) {
         super(props)
         this.iteratePins = this.iteratePins.bind(this) 
+        this.confirmDelete = this.confirmDelete.bind(this)
+        this.deleteAndRedirect = this.deleteAndRedirect.bind(this)
     }
+
+
+    deleteAndRedirect(board){
+        let _author_id = board.author_id
+        // let _history = this.props.history
+        debugger
+        // this.props.clearErrors()
+        this.confirmDelete(board, _author_id)
+        // .then(
+            // res => this.props.history.push(`/users/${author_id}`)
+        // )
+        // this.props.history.push(`/users/${author_id}`)
+            // .then(() => this.props.history.push(`/users/${author_id}`))
+            
+        // this.props.closeModal()
+    }
+
+
+    confirmDelete(board, _author_id){
+        Swal.fire({
+            title: 'Please confirm!',
+            text: 'Are you sure you want to delete this board?',
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes'
+        })
+        .then(function(result){
+            if (result.value){
+                $.ajax({
+                    url: `/api/users/${board.author_id}/boards/${board.id}`,
+                    method: "DELETE",
+                    dataType: 'json',
+                    beforeSend: function(){
+                        Swal.fire({
+                            title: 'Please Wait!!!',
+                            text: 'Deleting...',
+                            onOpen: function(){
+                                Swal.showLoading()
+                            }
+                        })
+                    },
+                    success : function(data){
+                        Swal.fire({
+                            type: 'success',
+                            title: 'Board deleted succesffully!',
+                            showConfirmationButton: false,
+                            timer: 2000
+                        }).then(function(){
+                            location.href=`/#/users/${_author_id}`;
+                        })
+                    },
+                    complete: function(){
+                        Swal.hideLoading();
+                    },
+                    error: function(jqXHR, textStatus, errThrown){
+                        Swal.hideLoading();
+                        Swal.fire('Oops!', 'Something went wrong! Try again!')
+                    }
+                }).then(
+                )
+            }
+        })
+    }
+        
 
 
     displayBoardDelete(author, currentUser, board){
         if ( author === currentUser ){
             return (
-                <div onClick={() => this.props.deleteBoard(author.id, board.id)}>
+                // <div onClick={() => this.props.deleteBoard(author.id, board.id)}>
+                // <div onClick={() => this.confirmDelete(author, board)}>
+                <div onClick={() => this.deleteAndRedirect(board)}>
                     Delete Board
                 </div>
             )
-
-        this.props.history.push(`/users/${author.id}`)
+            // .then(
+            // this.props.history.push(`/users/${author.id}`)
+            // )
         }
+        
     }
 
     iteratePins(thisBoardsPins){
@@ -58,19 +129,15 @@ class BoardShow extends React.Component {
 
 
     render(){
-
         let { board, author, currentUser, boardPins } = this.props
 
         let thisBoardsPins = Object.values(boardPins)
 
         if (board === undefined ) return null
         if (currentUser === undefined ) return null
-        if (thisBoardsPins.length === 0 ) return null
 
         return(
-            // <div className='content-container'>
             <div className='index-container'>
-                {/* <div className='board-show-container'> */}
                     <div className='board-show-det'>
                         <div id='board-title'>
                             {board.title}
@@ -86,13 +153,7 @@ class BoardShow extends React.Component {
                         </div>
                     </div>
 
-                    {/* <div className='board-show-items'> */}
-
-                {/* </div> */}
-                {/* <div className='pin-index-container'> */}
                     {this.iteratePins(thisBoardsPins)}
-                {/* </div> */}
-
             </div>
         )
     }
