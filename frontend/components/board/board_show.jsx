5bfc72fc0eb002481aa79e13d5d7ff68469e5551
Mac Mini <ryan@cygaq.com>
2020-12-26 19:31:15 -0800

    refactored board show

--- frontend/components/board/board_show.jsx
+++ frontend/components/board/board_show.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import PinIndexItem from '../pin/pin_index_item';
 import InlineDisplay from '../inline_display/inline_display_container'
-import { faVrCardboard } from '@fortawesome/free-solid-svg-icons';
-import board_create_container from './board_create_container';
+
 
 class BoardShow extends React.Component {
     constructor(props) {
@@ -10,15 +9,9 @@ class BoardShow extends React.Component {
         this.iteratePins = this.iteratePins.bind(this) 
     }
 
-    // componentDidMount() {
-    //     this.props.fetchPins()
-    //     this.props.fetchBoard(this.props.board_data)
-    //     // this.props.fetchUsers()
-    // }
 
     displayBoardDelete(author, currentUser, board){
         if ( author === currentUser ){
-        let authorId = author.id
             return (
                 <div onClick={() => this.props.deleteBoard(author.id, board.id)}>
                     Delete Board
@@ -30,7 +23,7 @@ class BoardShow extends React.Component {
     }
 
     iteratePins(thisBoardsPins){
-        debugger
+
         if (thisBoardsPins === null || thisBoardsPins.length === 0){
             return(
                 <div>
@@ -54,39 +47,11 @@ class BoardShow extends React.Component {
         }
     }
 
-    // render(){
-    //     debugger
-    //     let { board, thisBoardsPins, author, currentUser } = this.props;
-    //     if (author === null) return null;
-    //     if (board === undefined) return null;
-    //     // debugger
-
-    //     return(
-    //         <div className="content-container">
-    //             <div className='board-show-container'>
-    //                 <div className='board-show-det'>
-    //                     <ul>
-    //                         <li id='board-title'><InlineDisplay editType='board' item={board} objKey={'title'} field={board.title} /></li> */}
-    //                         <li id='board-owner'>board by {this.props.author.username}</li>
-    //                         <li>{this.displayBoardDelete(author, currentUser,board)}</li>
-    //                         <li id='board-description'><InlineDisplay editType='board' item={board} objKey={'description'} field={board.description} /></li>
-    //                     </ul>
-    //                 <div className='board-show-items'>
-    //                         {this.thisBoardsPins(pins_arr)}
-    //                         {this.iteratePins(thisBoardsPins)}
-    //                 </div>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     )
-    // }
-
     componentDidMount(){
         let boardData = {
             author_id: this.props.board_data.userId,
             id: this.props.board_data.boardId
         }
-        debugger
         this.props.fetchBoard(boardData)
         this.props.fetchBoardPins(boardData.id)
     }
@@ -101,10 +66,6 @@ class BoardShow extends React.Component {
         if (board === undefined ) return null
         if (currentUser === undefined ) return null
         if (thisBoardsPins.length === 0 ) return null
-        
-
-        debugger
-
 
         return(
             <div className='content-container'>
@@ -123,7 +84,6 @@ class BoardShow extends React.Component {
                             <InlineDisplay editType='board' item={board} objKey={'description'} field={board.description} />
                         </div>
                          <div className='board-show-items'>
-                             {/* {this.thisBoardsPins(pins_arr)} */}
                              {this.iteratePins(thisBoardsPins)}
                         </div>
                     </div>
