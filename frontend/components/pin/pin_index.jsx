caecaf9ef67b290c920c1747d542e8a2ccaa76da
Mac Mini <ryan@cygaq.com>
2020-12-28 12:13:44 -0800

    reinstalled packages and gems

--- frontend/components/pin/pin_index.jsx
+++ frontend/components/pin/pin_index.jsx
@@ -1,7 +1,6 @@
 
 import React from 'react'
 import PinIndexItem from './pin_index_item'
-import MagicGrid from 'react-magic-grid'
 
 class PinIndex extends React.Component {
     constructor(props) {
@@ -16,29 +15,10 @@ class PinIndex extends React.Component {
         const { pins } = this.props;
 
 
-//         const Cards = ({ cards }) => (
-//             <MagicGrid items={cards.length}>
-//                 {cards.map(item => (
-//       <Card key={item} item={item} />
-//     ))}
-//   </MagicGrid>
-// )
-
-        // let magicGrid = new MagicGrid({
-        //     container: '.pin-index-container',
-        //     items: pins.length,
-        //     animate: true,
-        //     gutter: 5,
-
-        // })
-
-        // magicGrid.listen();
-
         return (
             <div className="index-container">
                 <h1 id='header'>Latest Pins</h1>
                 <div className="pin-index-container">
-                    {/* <MagicGrid items={pins.length}> */}
                         {pins.reverse().map((pin) => (
                             <PinIndexItem
                                 pin={pin}
@@ -46,7 +26,6 @@ class PinIndex extends React.Component {
                                 photo={pin.photoUrl}
                             />
                         ))}
-                    {/* </MagicGrid>  */}
                 </div>
             </div>
         );
